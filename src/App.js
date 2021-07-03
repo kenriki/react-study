caebdf8d53ee02d082dfd4bca21affc8e1ca2469
kenriki <riki.kenmochi@gmail.com>
2021-07-03 23:56:08 +0900

    Aboutコンポーネント追加 first commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
 } from 'react-router-dom'
 import { CSVLink } from "react-csv";
 
+import { Grid } from '@material-ui/core';
+import About from './components/About';
 import Home from './pages/home/home.component'
 import Posts from './pages/posts/posts.component'
 import PostDetail from './pages/post-detail/post-detail.component'
@@ -19,6 +21,28 @@ const App = () => {
   return (
     <div className="App">
       <p>{`isAuthenticated: ${isAuthenticated}`}</p>
+      <Grid container direction="column">
+        <Grid item>
+          {/* <Header /> */}
+          <h1>test</h1>
+        </Grid>
+        <Grid item container>
+          <Grid sm={2} />
+          <Grid xs={12} sm={8}>
+            <Router>
+              <Switch>
+                <Route exact path="/">
+                  {/* <Content /> */}
+                </Route>
+                <Route path="/about">
+                  <About />
+                </Route>
+              </Switch>
+            </Router>
+          </Grid>
+          <Grid sm={2} />
+        </Grid>
+      </Grid>
       <Router>
         <div>
           <ul>
@@ -29,16 +53,16 @@ const App = () => {
             <li><Link to="/posts/2">Posts > 2</Link></li>
           </ul>
 
-          <hr/>
+          <hr />
 
           <Switch>
-            <Route exact path="/" component={Home}/>
+            <Route exact path="/" component={Home} />
             <Route exact path="/signin" render={props => isAuthenticated
-              ? (<Redirect to="/"/>)
-              : (<SignInAndSignUpPage {...props}/>)
-            }/>
-            <PrivateRoute exact path="/posts" component={Posts}/>
-            <PrivateRoute exact path="/posts/:id" component={PostDetail}/>
+              ? (<Redirect to="/" />)
+              : (<SignInAndSignUpPage {...props} />)
+            } />
+            <PrivateRoute exact path="/posts" component={Posts} />
+            <PrivateRoute exact path="/posts/:id" component={PostDetail} />
           </Switch>
         </div>
       </Router>
@@ -48,31 +72,31 @@ const App = () => {
 
 
 
- 
+
 const headers = [
   { label: "First Name", key: "firstname" },
   { label: "Last Name", key: "lastname" },
   { label: "Email", key: "email" }
 ];
- 
+
 const data = [
   { firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
   { firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
   { firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" }
 ];
- 
+
 
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
     <>
-    <Route {...rest} render={props => (isAuthenticated
+      <Route {...rest} render={props => (isAuthenticated
         ? <Component {...props} />
-        : <Redirect to="/signin"/>
-    )}/>
-    <CSVLink data={data} headers={headers} filename="download.csv">
-      Download me
-    </CSVLink>;
+        : <Redirect to="/signin" />
+      )} />
+      <CSVLink data={data} headers={headers} filename="download.csv">
+        Download me
+      </CSVLink>;
     </>
   )
 }
